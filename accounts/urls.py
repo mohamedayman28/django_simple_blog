bff3b5f8a73aecacb601948bac50212938754ad0
mohamedayman28 <b0ObAaao>
2021-05-04 12:16:18 +0200

    URLconf update

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='accounts_login'),
     path('logout/', LogoutView.as_view(next_page='post_home_page'), name='accounts_logout'),
     path('signup/', views.user_form, name='accounts_signup'),
-    path('user-update/<id>/', views.user_form, name='accounts_user_update'),
+    path('user-update/<int:id>/', views.user_form, name='accounts_user_update'),
 ]
--- posts/urls.py
+++ posts/urls.py
@@ -13,13 +13,13 @@ urlpatterns = [
     # Create new post.
     path('create-post/', views.post_form, name='post_create'),
     # Get post details.
-    path('post/<id>/', views.post_details, name='post_details'),
+    path('post/<int:id>/', views.post_details, name='post_details'),
     # Create comment related to post.
-    path('post/<id>/create-comment/', views.post_comment_create, name='post_comment_create'),
+    path('post/<int:id>/create-comment/', views.post_comment_create, name='post_comment_create'),
     # Update post.
-    path('update-post/<id>/', views.post_form, name='post_update'),
+    path('update-post/<int:id>/', views.post_form, name='post_update'),
     # Delete post
-    path('delete-post/<id>/', views.post_form, name='post_delete'),
+    path('delete-post/<int:id>/', views.post_form, name='post_delete'),
     # Filter posts by category.
-    path('category/<category>/', views.home_page, name='post_category'),
+    path('category/<str:category>/', views.home_page, name='post_category'),
 ]
